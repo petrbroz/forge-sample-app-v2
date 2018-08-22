c4e5e175f7c4dcdf3ff666be871128d052c4c155
Petr Broz <petr.broz@autodesk.com>
2018-08-22 11:13:45 +0200

    Added early exit when env vars are not provided.

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const path = require('path');
 const express = require('express');
 
 const PORT = process.env.PORT || 3000;
+const config = require('./config');
+if (config.credentials.client_id == null || config.credentials.client_secret == null) {
+    console.error('Missing FORGE_CLIENT_ID or FORGE_CLIENT_SECRET env. variables.');
+    return;
+}
 
 let app = express();
 app.use(express.static(path.join(__dirname, 'public')));
