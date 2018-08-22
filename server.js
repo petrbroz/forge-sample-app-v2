b05ef70e789e974f7393cce49450c1e741454a0c
Petr Broz <petr.broz@autodesk.com>
2018-08-22 11:49:41 +0200

    Added default error handler.

--- server.js
+++ server.js
@@ -14,4 +14,8 @@ app.use(express.json({ limit: '50mb' }));
 app.use('/api/forge/oauth', require('./routes/auth'));
 app.use('/api/forge/oss', require('./routes/oss'));
 app.use('/api/forge/modelderivative', require('./routes/jobs'));
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json(err);
+});
 app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`); });
